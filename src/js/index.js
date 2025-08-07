3ca5e949b296aa8983eea9cb90e0d90278a31788
alissondwirvelle <alissondwirvelle7@gmail.com>
2025-08-07 15:50:13 -0300

    adiciona a funcionalidade que monta a tabela do carrinho

--- src/js/index.js
+++ src/js/index.js
@@ -46,6 +46,7 @@ botoesAdicionarAoCarrinho.forEach(botao => {
 
         salvarProdutosNoCarrinho(carrinho);
         atualizarContadorCarrinho();
+        renderizarTabelaDoCarrinho();
     });
 });
 
@@ -60,10 +61,10 @@ function obterProdutosDoCarrinho() {
 
 //passo 4 - atuaizar o contador do carrinho de compras
 function atualizarContadorCarrinho() {
-    const carrinho = obterProdutosDoCarrinho();
+    const produtos = obterProdutosDoCarrinho();
     let total = 0;
 
-    carrinho.forEach(produto => {
+    produtos.forEach(produto => {
         total += produto.quantidade;
     });
 
@@ -71,3 +72,28 @@ function atualizarContadorCarrinho() {
 }
 
 atualizarContadorCarrinho();
+
+//passo 5 - atualizar a tabela do carrinho
+function renderizarTabelaDoCarrinho() {
+    const produtos = obterProdutosDoCarrinho();
+    const corpoTabela = document.querySelector("#modal-1-content table tbody");
+    corpoTabela.innerHTML = ""; // Limpa a tabela antes de renderizar
+
+    produtos.forEach(produto => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `<td class="td-produto">
+        <img 
+            src="${produto.imagem}"
+            alt="${produto.nome}"
+        />
+        </td>
+        <td>${produto.nome}</td>
+             <td class="td-preco-unitario">R$ ${produto.preco.toFixed(2).replace(".", ",")}</td>
+             <td class="td-quantidade"><input type="number" value= "${produto.quantidade}" min="1"></td>
+             <td class="td-preco-total">R$ ${produto.preco.toFixed(2).replace(".", ",")}</td>
+             <td><button class="btn-deletar" data-id="${produto.id}" id="deletar"></button></td>`;
+        corpoTabela.appendChild(tr);
+    });
+}
+
+renderizarTabelaDoCarrinho();
